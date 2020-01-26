e721943f0397ba9b24b54b5c7cc43d1b49670516
Jelle Adan <jelle.adan@protonmail.com>
2020-01-26 21:02:32 +0100

    Add files via upload

---
+++ Queuing_Simulation_2/Objects/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace Queuing_Simulation_2.Objects
+{
+    public class Customer
+    {
+        public int cID { get; set; }
+        public double arrivalTime { get; set; }
+        public double departureTime { get; set; }
+        public double serviceTime { get; set; }
+        public Server server { get; set; }
+
+        public Customer(int cID)
+        {
+            this.cID = cID;
+        }
+    }
+}
---
+++ Queuing_Simulation_2/Objects/Server.cs
@@ -0,0 +1,14 @@
+﻿namespace Queuing_Simulation_2.Objects
+{
+    public class Server
+    {
+        public int sID { get; set; }
+        public bool[] eligibility { get; set; }
+
+        public Server(int sID, bool[] eligibility)
+        {
+            this.sID = sID;
+            this.eligibility = eligibility;
+        }
+    }
+}
